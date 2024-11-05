c5167a586141f2e6a1e6493f29371089e4a08583
Renold Thomas <renoldthomasp@gmail.com>
2024-11-05 11:30:48 -0500

    Create skeleton files and basic functions for js
    I created the basic functions needed for the library script file. I
    created an addToLibrary function and a Book constructor to be able to
    achieve this.

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
+
+function addBookToLibrary(title, author, pages) {
+    let book = new Book(title, author, pages);
+    myLibrary.push(book);
+}
+
