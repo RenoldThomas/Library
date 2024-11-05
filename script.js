8b58f923fab10bc6649dbc8159d371b05bcec655
Renold Thomas <renoldthomasp@gmail.com>
2024-11-05 11:39:40 -0500

    Edit script file to take read or not read input

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+
+    if (read.toLowerCase() === "yes") {
+        this.read = "has read";
+    }
+    else {
+        this.read = "hasn't read yet";
+    }
 }
 
-function addBookToLibrary(title, author, pages) {
-    let book = new Book(title, author, pages);
+function addBookToLibrary(title, author, pages, read) {
+    let book = new Book(title, author, pages, read);
     myLibrary.push(book);
 }
 
