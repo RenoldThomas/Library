626bc7150472bb8d9e9775a4d9d3f5eb8ec2c4a3
Renold Thomas <renoldthomasp@gmail.com>
2024-11-06 16:35:17 -0500

    Made code create new rows with new book
    I created a button that prompted the user to enter information of any
    book and add display the book in the table.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
 
-    if (read.toLowerCase() === "yes") {
+    if (read) {
         this.read = "has read";
     }
     else {
@@ -16,16 +16,31 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
     let book = new Book(title, author, pages, read);
     myLibrary.push(book);
+    // console.log(myLibrary);
+    displayLibrary(myLibrary);
+}
+
+function clearTableBody() {
+    const table = document.querySelector(".library");
+    const rows = table.querySelectorAll("tr:not(:first-child)"); // Select all rows except the first (header)
+
+    rows.forEach(row => row.remove());
 }
 
 function displayLibrary(library) {
+    clearTableBody();
     let table = document.querySelector(".library");
+
     for (let i = 0; i < library.length; i++) {
-        let newRow = table.insertRow(-1);
-        newRow.insertCell(0).innerHTML = library[i].title;
-        newRow.insertCell(1).innerHTML = library[i].author;
-        newRow.insertCell(2).innerHTML = library[i].pages;
-        newRow.insertCell(3).innerHTML = library[i].read;
+        let existingRow = table.querySelector(`tr:nth-child(${i + 1})`);
+
+        if (!existingRow || existingRow.cells[0].textContent !== library[i].title) {
+            let newRow = table.insertRow(-1);
+            newRow.insertCell(0).innerHTML = library[i].title;
+            newRow.insertCell(1).innerHTML = library[i].author;
+            newRow.insertCell(2).innerHTML = library[i].pages;
+            newRow.insertCell(3).innerHTML = library[i].read;
+        }
     }
 
     // Select all the cells in the table
@@ -40,8 +55,46 @@ function displayLibrary(library) {
     }
 }
 
-addBookToLibrary("thefix", "david baldacci", 324, "yes");
-addBookToLibrary("harrypotter", "jkrowling", 500, "no");
-addBookToLibrary("cat in the hat", "DR. Seuss", 38, "yes");
+// Dialog to add new book
+let dialogAddBook = document.querySelector("dialog");
+
+function showDialog() {
+    dialogAddBook.showModal();
+}
+
+function closeAdd() {
+    dialogAddBook.close();
+}
+
+// Receive form data and create book object
+let formNewBook = document.querySelector("form");
+let cancelButton = document.getElementById("cancelAdd");
+
+cancelButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    dialogAddBook.close();
+    formNewBook.reset();
+})
+
+formNewBook.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // Grab form entries and place in FormData object
+    const bookData = new FormData(formNewBook);
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let read = document.querySelector("#read").checked;
+
+    // Create new book from the FormData object and add to library
+    addBookToLibrary(title, author, pages, read);
+    dialogAddBook.close();
+
+    formNewBook.reset();
+})
+
+addBookToLibrary("The Fix", "David Baldacci", 432, true);
+addBookToLibrary("Harry Potter and the Sorcerer's Stone", "J. K. Rowling", 309, false);
+addBookToLibrary("The Cat In the Hat", "Dr. Seuss", 61, true);
 
 displayLibrary(myLibrary);
